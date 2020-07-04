d7a5d56a251d1be3286b28e77763bba196a5eee1
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-07-04 14:23:08 +0300

    Add an event listener for the submit buton

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const formCheckBox = document.getElementsByClassName('form-check');
 const hobbiesCheckBox = document.querySelector('.form-check-inline');
 const submitButton = document.querySelector('#submit-form');
 const postList = document.querySelector('#post-list')
-
+const postTitle = document.querySelector('#new-post-title')
+const postContent = document.querySelector('#new-post-content')
 
 const divCard = document.createElement('div');
 const divCardBody = document.createElement('div');
@@ -12,13 +13,21 @@ const newParagraph = document.createElement('p');
 
 divCard.classList.add('card', 'some-space');
 divCardBody.classList.add('card-body');
-newheading.classList.add('card-title')
-newParagraph.classList.add('card-text')
+newheading.classList.add('card-title');
+newParagraph.classList.add('card-text');
+
+
+submitButton.addEventListener('click', ($event) => {
+    $event.preventDefault();
+    newheading.textContent = `${postTitle.value}`;
+    newParagraph.textContent = `${postContent.value}`;
+
+});
+
 
 divCardBody.appendChild(newheading);
 divCardBody.appendChild(newParagraph);
 divCard.appendChild(divCardBody);
-
-
+postList.appendChild(divCardBody);
 
 
