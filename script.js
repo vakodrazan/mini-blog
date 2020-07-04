4a654f70fbf712dcfd2d5f82eb3bb62645cc8938
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-07-04 11:17:29 +0300

    Create variables that contains the sector for an element

---
+++ script.js
@@ -0,0 +1,4 @@
+const formCheckBox = document.getElementsByClassName('form-check');
+const hobbiesCheckBox = document.querySelector('#hobbies-tag');
+const submitButton = document.querySelector('#submit-form');
+const container = document.querySelector('.container')
