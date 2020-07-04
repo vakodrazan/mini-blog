f00b661f43a0053c59615c65f1421198c6359c1b
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-07-04 11:29:26 +0300

    Create three elements

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+
 const formCheckBox = document.getElementsByClassName('form-check');
-const hobbiesCheckBox = document.querySelector('#hobbies-tag');
+const hobbiesCheckBox = document.querySelector('.form-check-inline');
 const submitButton = document.querySelector('#submit-form');
 const container = document.querySelector('.container')
+
+
+const newDiv = document.createElement('div');
+const newheading = document.createElement('h5');
+const newParagraph = document.createElement('p');
+
