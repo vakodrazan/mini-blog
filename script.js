07e03893403ddaa50a0539b31ebd07f877d705d9
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-07-04 14:57:24 +0300

    Nest the new element in the exists parent div inside of the event listener

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
-
+// Create a variable for each element
 const formCheckBox = document.getElementsByClassName('form-check');
 const hobbiesCheckBox = document.querySelector('.form-check-inline');
 const submitButton = document.querySelector('#submit-form');
-const postList = document.querySelector('#post-list')
-const postTitle = document.querySelector('#new-post-title')
-const postContent = document.querySelector('#new-post-content')
+const postList = document.querySelector('#post-list');
+const postTitle = document.querySelector('#new-post-title');
+const postContent = document.querySelector('#new-post-content');
+const form = document.querySelector('form');
 
+// Create elements
 const divCard = document.createElement('div');
 const divCardBody = document.createElement('div');
 const newheading = document.createElement('h5');
 const newParagraph = document.createElement('p');
 
+// Add class to each new element
 divCard.classList.add('card', 'some-space');
 divCardBody.classList.add('card-body');
 newheading.classList.add('card-title');
-newParagraph.classList.add('card-text');
+newParagraph.classList.add('card-text')
 
+// Nesting element
+divCardBody.appendChild(newheading);
+divCardBody.appendChild(newParagraph);
+divCard.appendChild(divCardBody);
 
+// Create an event listener for the submit button
 submitButton.addEventListener('click', ($event) => {
     $event.preventDefault();
     newheading.textContent = `${postTitle.value}`;
     newParagraph.textContent = `${postContent.value}`;
-
+    postList.appendChild(divCard);
+    form.reset()
 });
-
-
-divCardBody.appendChild(newheading);
-divCardBody.appendChild(newParagraph);
-divCard.appendChild(divCardBody);
-postList.appendChild(divCardBody);
 
 
